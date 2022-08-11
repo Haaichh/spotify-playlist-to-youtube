be361c1a9effd271c008cd2f9e6885672f7e291e
Holly Reed <2001hollyreed@gmail.com>
2022-08-11 11:39:32 +0100

    Convert to YouTube playlist, exceeds quota

--- spotify_playlist_to_youtube.py
+++ spotify_playlist_to_youtube.py
@@ -9,8 +9,7 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-scope = 'user-library-read'
-
+# Checks .py is run with two arguments: Spotify username and the link to Spotify playlist
 if len(sys.argv) > 2:
     username = sys.argv[1]
     playlist = sys.argv[2]
@@ -18,8 +17,11 @@ else:
     print("Usage: %s username playlist" % (sys.argv[0]))
     sys.exit()
 
+# Spotify API
+scope = 'user-library-read'
 token = util.prompt_for_user_token(username, scope)
 
+# Creates list of song titles and artist names
 if token:
     sp = spotipy.Spotify(auth=token)
     results = sp.playlist_tracks(playlist)
@@ -37,21 +39,18 @@ if token:
 else:
     print("Can't get token for", username)
 
-
+# YouTube API
 api_service_name = "youtube"
 api_version = "v3"
 client_secrets_file = "client_secret_file.json"
 scopes = 'https://www.googleapis.com/auth/youtube'
-
 flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
     client_secrets_file, scopes)
 credentials = flow.run_console()
 youtube = googleapiclient.discovery.build(
     api_service_name, api_version, credentials=credentials)
 
-request = youtube.search().list(q='CIX - Movie Star',part='snippet',type='video', maxResults=1)
-response = request.execute()
-
+# Creates a YouTube playlist
 request = youtube.playlists().insert(
     part="snippet,status",
     body={
@@ -65,30 +64,25 @@ request = youtube.playlists().insert(
     }
 )
 response = request.execute()
+iDresponse = response['id']
 
-
-""" get playlist ID
-request = youtube.playlists().list(
-    part="snippet,contentDetails",
-    maxResults=1,
-    mine=True
-)
-response = request.execute()
-"""
-
-""" add song to playlist
-request = youtube.playlistItems().insert(
-    part="snippet",
-    body={
-        "snippet": {
-        "playlistId": "YOUR_PLAYLIST_ID",
-        "position": 0,
-        "resourceId": {
-            "kind": "youtube#video",
-            "videoId": response['items'][0]['id']['videoId']
+# For each song, searches YouTube, then adds to the playlist
+j = 0
+for song in songs:
+    request = youtube.search().list(q=song,part='snippet',type='video', maxResults=1)
+    response = request.execute()
+    request = youtube.playlistItems().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "playlistId": iDresponse,
+                "position": j,
+                "resourceId": {
+                    "kind": "youtube#video",
+                    "videoId": response['items'][0]['id']['videoId']
+                }
+            }
         }
-        }
-    }
-)
-response = request.execute()
-"""
+    )
+    response = request.execute()
+    j += 1
