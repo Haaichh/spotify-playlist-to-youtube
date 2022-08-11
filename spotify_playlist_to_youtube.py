314f8e2ca2c64474e0985e791986d67fc7e1a287
Holly Reed <2001hollyreed@gmail.com>
2022-08-11 10:49:41 +0100

    Create YouTube playlist

--- spotify_playlist_to_youtube.py
+++ spotify_playlist_to_youtube.py
@@ -1,7 +1,13 @@
+# this is still such a mess please do not look at this code
+
 from __future__ import print_function
 import sys
 import spotipy
 import spotipy.util as util
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 
 scope = 'user-library-read'
 
@@ -31,4 +37,58 @@ if token:
 else:
     print("Can't get token for", username)
 
-print(songs, len(songs))
+
+api_service_name = "youtube"
+api_version = "v3"
+client_secrets_file = "client_secret_file.json"
+scopes = 'https://www.googleapis.com/auth/youtube'
+
+flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    client_secrets_file, scopes)
+credentials = flow.run_console()
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, credentials=credentials)
+
+request = youtube.search().list(q='CIX - Movie Star',part='snippet',type='video', maxResults=1)
+response = request.execute()
+
+request = youtube.playlists().insert(
+    part="snippet,status",
+    body={
+        "snippet": {
+        "title": "Spotify Playlist to YouTube",
+        "defaultLanguage": "en"
+        },
+        "status": {
+        "privacyStatus": "private"
+        }
+    }
+)
+response = request.execute()
+
+
+""" get playlist ID
+request = youtube.playlists().list(
+    part="snippet,contentDetails",
+    maxResults=1,
+    mine=True
+)
+response = request.execute()
+"""
+
+""" add song to playlist
+request = youtube.playlistItems().insert(
+    part="snippet",
+    body={
+        "snippet": {
+        "playlistId": "YOUR_PLAYLIST_ID",
+        "position": 0,
+        "resourceId": {
+            "kind": "youtube#video",
+            "videoId": response['items'][0]['id']['videoId']
+        }
+        }
+    }
+)
+response = request.execute()
+"""
